ad958f058e8ad378a64083018e902fd50a95913a
Kamal A <kamal.anbazhagan@cdw.com>
2026-03-23 00:52:29 +0530

    Update paths in assignment1.sh to use relative directory

--- assignment1.sh
+++ assignment1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-mkdir -p ~/project_files
-find /var/log -type f -size +50k -exec cp {} ~/project_files/ \;
-chmod 640 ~/project_files/*
-du -sh ~/project_files
+
+mkdir -p project_files
+find /var/log -type f -size +50k -exec cp {} project_files/ \;
+chmod 640 project_files/*
+du -sh project_files
